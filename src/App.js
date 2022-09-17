51abc3893160eeeaab569839e6926dbbb5f0084a
Rahul Raut <rahulraut1996@gmail.com>
2022-09-17 18:07:36 +0530

    HomePageCompleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './HomeComponents/Home';
+import FetchDiscription from './Description1/FetchDiscription';
+import Cart2 from './Components/Cart2';
+//import NavigationBar from './NavBar/NavigationBar'
+import NavigationBar1 from './NavBar/NavigationBar1';
+import Customer from './Registration/Customer'
+import LoginValidation from './Shelf/login';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      {/* <LoginValidation/> */}
+       <Home/>
+      {/* <FetchDiscription/> */}
+      {/* <Customer/> */}
+      {/* <NavigationBar1/> */}
+     
+      {/* <Cart2/> */}
+      {/* <Customer/> */}
     </div>
   );
 }
